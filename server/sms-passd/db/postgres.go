6b436338a98aec31b12b239f226aed42fd54dac4
aavzz <aavzz@yandex.ru>
2018-01-13 04:15:20 +0400

    Update postgres.go

--- server/sms-passd/db/postgres.go
+++ server/sms-passd/db/postgres.go
@@ -10,10 +10,10 @@ var dbh *sql.DB
 
 func InitDB() {
 	var err error
-	if dbh, err := sql.Open("postgres", "host=/var/run/postgresql user=foo password=asd dbname=bar sslmode=disable"); err != nil {
+	if dbh, err = sql.Open("postgres", "host=/var/run/postgresql user=foo password=asd dbname=bar sslmode=disable"); err != nil {
 		log.Fatal(err.Error())
 	}
-	if err := dbh.Ping(); err != nil {
+	if err = dbh.Ping(); err != nil {
 		if err := dbh.Close(); err != nil {
 			log.Fatal(err.Error())
 		}
