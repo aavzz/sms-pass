91ac47ad116c265fe55a1e33d474540e587740f8
aavzz <aavzz@yandex.ru>
2018-01-20 11:23:10 +0400

    Update spa.go

--- server/sms-passd/rest/spa.go
+++ server/sms-passd/rest/spa.go
@@ -14,8 +14,8 @@ const startPage = `<!DOCTYPE HTML>
     <script src="/assets/w2ui/libs/jquery/jquery-2.1.0.min.js"></script>
     <script src="/assets/jQuery-Mask-Plugin/dist/jquery.mask.min.js"></script>
     <script src="/assets/w2ui/dist/w2ui.min.js"></script>
-    <script src="/assets/app-strings.js"></script>
-    <script src="/assets/app.js"></script>
+    <script src="/assets/sms-pass/app-strings.js"></script>
+    <script src="/assets/sms-pass/app.js"></script>
 </head>
 <body>
     <div id="root" style="width: 445px; height: 700px; display: block; margin-left: auto; margin-right: auto;"></div>
