a3246e7f5bcfaa3ad63cecf757c6871ad5781deb
aavzz <aavzz@yandex.ru>
2018-02-01 20:27:39 +0400

    Update app-conf.js

--- htdocs/sms-pass/app-conf.js
+++ htdocs/sms-pass/app-conf.js
@@ -5,19 +5,19 @@ let appConfig = {
     phoneMask: "+7 (9ZZ) ZZZ-ZZ-ZZ"
     phonePlaceholder: "9XX XXX XX XX"
     isp: {
-        name: "ISP name";
-        logo" "path to ISP logo";
+        name: "ISP name",
+        logo" "path to ISP logo",
         logoWidth: 100,
         logoHeight: 100,
     },
     hotspot: {
-        type: "mikrotik|test";
-        name: "Acme inc.";
-        logo: "path to client logo";
+        type: "mikrotik|test",
+        name: "Acme inc.",
+        logo: "path to client logo",
         logoWidth: 100,
         logoHeight: 100,
-        urlA: "authentication URL";
-        urlR: "client start page";
+        urlA: "authentication URL",
+        urlR: "client start page",
     },
 };
 
