a6ede7fb1c1002eec86b1b832efee56ff9782691
aavzz <aavzz@yandex.ru>
2018-02-02 01:10:18 +0400

    Create notify.go

---
+++ server/sms-passd/timer/notify.go
@@ -0,0 +1,15 @@
+package timer
+
+import (
+  "time"
+)
+
+var NotificationAllowed = true
+
+func AllowNotification() {
+  for {
+    time.Sleep(3600 * time.Second)
+    NotificationAllowed = true
+  }
+}
+
