6497876dcb74ac1c3a408d9977df509aba9f0ca0
aavzz <aavzz@yandex.ru>
2018-02-02 05:04:20 +0400

    Update notify.go

--- server/sms-passd/timer/notify.go
+++ server/sms-passd/timer/notify.go
@@ -1,11 +1,17 @@
+/*
+Package timer guards against too frequent notifications
+*/
+
 package timer
 
 import (
 	"time"
 )
 
+// NotificationAllowed indicates if we can send notifications
 var NotificationAllowed = true
 
+// AllowNotification resets NotificationAllowed to `true` every hour 
 func AllowNotification() {
 	for {
 		time.Sleep(3600 * time.Second)
