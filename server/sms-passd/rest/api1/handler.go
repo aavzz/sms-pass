5a488e7581792647540d7ab1386f9f0e56de1315
aavzz <aavzz@yandex.ru>
2018-02-02 06:34:25 +0400

    Update handler.go

--- server/sms-passd/rest/api1/handler.go
+++ server/sms-passd/rest/api1/handler.go
@@ -65,26 +65,12 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	re = regexp.MustCompile(`\.`)
 	clientSection := re.ReplaceAllString(p, "_")
 
-	startPage, err := ioutil.ReadFile(viper.GetString("sms-passd.assets") + "/" + viper.GetString(clientSection+".assets") + "/spa.html")
-	if err != nil {
+	if viper.IsSet(clientSection+".assets") == false {
 		http.Redirect(w, r, viper.GetString("sms-passd.redirect"), 301)
 	}
 	
 	ret := json.NewEncoder(w)
 	operation := r.FormValue("operation")
-
-	// figure out client prefix and get client configuration
-	header := viper.GetString("sms-passd.real_ip_header")
-	var clientIp string
-	if header == "" || header == "none" {
-		clientIp = r.RemoteAddr
-	} else {
-		clientIp = r.Header.Get(header)
-	}
-	re := regexp.MustCompile(`^\d+\.\d+\.\d+`)
-	p := re.FindString(clientIp)
-	re = regexp.MustCompile(`\.`)
-	clientSection := re.ReplaceAllString(p, "_")
 
 	switch operation {
 	case "pass":
