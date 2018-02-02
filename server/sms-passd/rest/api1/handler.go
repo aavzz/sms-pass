7c8d31cf06ed50bcdf27189d66fc8e5fef17ac71
aavzz <aavzz@yandex.ru>
2018-02-02 06:19:37 +0400

    Update handler.go

--- server/sms-passd/rest/api1/handler.go
+++ server/sms-passd/rest/api1/handler.go
@@ -51,6 +51,25 @@ type PassResp struct {
 // Handler processes requests and responds with a JSON object.
 func Handler(w http.ResponseWriter, r *http.Request) {
 
+	// redirect unknown clients
+	// figure out client prefix and get client configuration
+	header := viper.GetString("sms-passd.real_ip_header")
+	var clientIp string
+	if header == "" || header == "none" {
+		clientIp = r.RemoteAddr
+	} else {
+		clientIp = r.Header.Get(header)
+	}
+	re := regexp.MustCompile(`^\d+\.\d+\.\d+`)
+	p := re.FindString(clientIp)
+	re = regexp.MustCompile(`\.`)
+	clientSection := re.ReplaceAllString(p, "_")
+
+	startPage, err := ioutil.ReadFile(viper.GetString("sms-passd.assets") + "/" + viper.GetString(clientSection+".assets") + "/spa.html")
+	if err != nil {
+		http.Redirect(w, r, viper.GetString("sms-passd.redirect"), 301)
+	}
+	
 	ret := json.NewEncoder(w)
 	operation := r.FormValue("operation")
 
