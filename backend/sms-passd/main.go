3a78ea45eaf88c025822fac6dbca2b1c483de89d
aavzz <aavzz@yandex.ru>
2018-02-03 22:01:48 +0400

    Update main.go

--- backend/sms-passd/main.go
+++ backend/sms-passd/main.go
@@ -4,7 +4,7 @@ sms-passd generates a password and sends it via SMS using notifier service
 package main
 
 import (
-	"github.com/aavzz/sms-pass/server/sms-passd/cmd"
+	"github.com/aavzz/sms-pass/backend/sms-passd/cmd"
 	"math/rand"
 	"time"
 )
